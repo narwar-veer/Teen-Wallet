90a403bc05f1a2e4dc58e4fe80b6931f7a2a6b64
narwar-veer <narwarveer108@gmail.com>
2025-07-26 09:35:12 +0530

    Initial commit

---
+++ cmd/server/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "log"
+
+    "github.com/narwar-veer/teen-wallet-api/internal/config"
+    "github.com/narwar-veer/teen-wallet-api/internal/database"
+    "github.com/narwar-veer/teen-wallet-api/internal/router"
+)
+
+func main() {
+    cfg := config.MustLoad()
+
+    db := database.MustConnect(cfg)
+    database.AutoMigrate(db)
+
+    r := router.New(cfg, db)
+
+    log.Printf("🚀  Server starting on %s", cfg.HTTPServer.Addr)
+    if err := r.Run(cfg.HTTPServer.Addr); err != nil {
+        log.Fatalf("unable to start server: %v", err)
+    }
+}
---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/narwar-veer/teen-wallet-api
+
+go 1.24.5
+
+
+require (
+    github.com/gin-gonic/gin v1.10.1 
+    github.com/ilyakaznacheev/cleanenv v1.5.0
+    github.com/golang-jwt/jwt/v5 v5.2.0
+    gorm.io/driver/postgres v1.6.0
+    gorm.io/gorm v1.30.1
+    github.com/go-playground/validator/v10 v10.22.0
+)
